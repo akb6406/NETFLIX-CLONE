0756f02430b1b25c8026a65b295594180aa85628
Ankit Kumar <89263177+akb6406@users.noreply.github.com>
2024-06-17 14:43:46 +0530

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,5 @@
-export const API_KEY = "cfc5b86d9cc077f683dc6cb2e90c41d2";
-export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
+
+export const API_KEY = "b75fe950";
+export const TMDB_BASE_URL = "http://www.omdbapi.com";
+
+
